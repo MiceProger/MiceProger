f87250a2cddfb0c8bbad547a4fad48bbf8c5bd45
MiceProger <sabovmicha@gmail.com>
2022-05-31 20:39:26 +0300

    'fix'

--- src/main/java/com/michael/MyVocabulary/controller/RegistrationController.java
+++ src/main/java/com/michael/MyVocabulary/controller/RegistrationController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
