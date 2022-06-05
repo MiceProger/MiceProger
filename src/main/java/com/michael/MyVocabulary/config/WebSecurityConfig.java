17cc1ebd765bd1cc81b94d4aa712aa975d49f495
MiceProger <sabovmicha@gmail.com>
2022-06-05 13:46:33 +0300

    'fix'

--- src/main/java/com/michael/MyVocabulary/config/WebSecurityConfig.java
+++ src/main/java/com/michael/MyVocabulary/config/WebSecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,13 +18,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http)
             throws Exception {
-        http.cors().and().csrf().disable();
-        http.authorizeRequests().antMatchers("/vocabularyWords", "/login").fullyAuthenticated().and().httpBasic();
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/vocabularyWords" ).fullyAuthenticated().and().httpBasic();
     }
 
     @Bean
     protected PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:8080", "https://miceproger.github.io/", " https://damp-dawn-99093.herokuapp.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST","OPTIONS", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization","content-type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
